5bf9b618a306cfede10f53a972dcb62b2818ea3f
Ebikabowei Dazi <isaacdazi00@gmail.com>
2023-08-09 14:08:38 -0700

    Replace the old files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.scss';
+//import './styles/index.scss';
 import App from './components2/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
