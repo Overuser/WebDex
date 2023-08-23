c21297d280991b53ab4773e99010fd5320c342c4
Ebikabowei Dazi <isaacdazi00@gmail.com>
2023-08-23 12:30:55 +0100

    change file structure #3

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
 
 export default function RootLayout() {
   return (
     <div className='app'>
       <header className='app__header'>
-        <Navbar />
       </header>
       <main className='app__main'>
         <Outlet />
---
+++ src/pages/home/componenets/HomeDex.js
@@ -0,0 +1,13 @@
+import { DexBlock } from "../../components/Block";
+
+const DexBlocks = ({ dexData, ModalView }) => {
+  return (
+    <div className='home__dex'>
+      {dexData.map((dex) => {
+        return <DexBlock key={dex.id} dex={dex} ModalView={ModalView} />;
+      })}
+    </div>
+  );
+};
+
+export default DexBlocks;
